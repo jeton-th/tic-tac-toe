e9114b7892e50bdbbaa38411b186784b32c7a785
Jeton Thaçi <jetonthaci@outlook.com>
2019-03-21 17:47:10 +0100

    add message and play defs

--- game.rb
+++ game.rb
@@ -1,9 +1,17 @@
 class Game
-  @@board = [
+  @@board = {
     [' - ', ' - ', ' - '],
     [' - ', ' - ', ' - '],
     [' - ', ' - ', ' - ']
-  ]
+  }
+
+  @@turn = 1
+  @@game_over = false
+  @@winner = 0
+
+  def initialize
+      message
+  end
 
   def show_board
     puts ''
@@ -20,10 +28,39 @@ class Game
     puts '  '
   end
 
-  def play(player, move)
+  def message
+    if @@game_over
+      show_board
+      puts "Game over. Player #{@@winner} wins."
+      puts ''
+    else
+      show_board
+      puts "Insert field to play e.g. a1"
+      print "Player #{@@turn}: "
+      field = gets.chomp
+      play(field)
+    end
+  end
+  
+  def play(field)
+    if (field.length == 2 &&
+      "abc".include?(field[0]) &&
+      "123".include?(field[1]))
+      insert(field)
+    else
+      puts "That field does not exist"
+      message
+    end
+  end
+
+  def insert(field)
+    letter = field[0]
+    number = field[1]
+
+    
+
   end
   
 end
 
 game = Game.new
-game.show_board
