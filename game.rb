82dbc656d9022e85fbccdbd4ba0b19b53eedfdb0
Jeton Thaçi <jetonthaci@outlook.com>
2019-03-22 14:03:33 +0100

    fix some linting errors

--- game.rb
+++ game.rb
@@ -1,54 +1,50 @@
+# Tic Tac Toe Game Class
 class Game
-  @@board = {
-    [' - ', ' - ', ' - '],
-    [' - ', ' - ', ' - '],
-    [' - ', ' - ', ' - ']
-  }
-
-  @@turn = 1
-  @@game_over = false
-  @@winner = 0
-
   def initialize
-      message
+    @board = [
+      [' - ', ' - ', ' - '],
+      [' - ', ' - ', ' - '],
+      [' - ', ' - ', ' - ']
+    ]
+
+    @turn = 1
+    @game_over = false
+    @winner = 0
+
+    message
   end
 
   def show_board
-    puts ''
-    puts '      a     b     c     '
+    puts "\n      a     b     c     "
     puts '         |     |     '
-    puts '  1  ' + @@board[0].join(' | ')
+    puts '  1  ' + @board[0].join(' | ')
     puts '    _____|_____|_____'
     puts '         |     |     '
-    puts '  2  ' + @@board[1].join(' | ')
+    puts '  2  ' + @board[1].join(' | ')
     puts '    _____|_____|_____'
     puts '         |     |     '
-    puts '  3  ' + @@board[2].join(' | ')
-    puts '         |     |     '
-    puts '  '
+    puts '  3  ' + @board[2].join(' | ')
+    puts "         |     |     \n"
   end
 
   def message
-    if @@game_over
-      show_board
-      puts "Game over. Player #{@@winner} wins."
+    show_board
+    if @game_over
+      puts "Game over. Player #{@winner} wins."
       puts ''
     else
-      show_board
-      puts "Insert field to play e.g. a1"
-      print "Player #{@@turn}: "
+      puts 'Insert field to play e.g. a1'
+      print "Player #{@turn}: "
       field = gets.chomp
       play(field)
     end
   end
-  
+
   def play(field)
-    if (field.length == 2 &&
-      "abc".include?(field[0]) &&
-      "123".include?(field[1]))
+    if field.length == 2 && 'abc'.include?(field[0]) && '123'.include?(field[1])
       insert(field)
     else
-      puts "That field does not exist"
+      puts 'That field does not exist'
       message
     end
   end
@@ -56,11 +52,7 @@ class Game
   def insert(field)
     letter = field[0]
     number = field[1]
-
-    
-
   end
-  
 end
 
-game = Game.new
+Game.new
