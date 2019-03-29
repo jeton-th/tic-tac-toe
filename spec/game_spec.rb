f64adbf16ac442f2bf1261d73f1a8dc169a0669e
Jeton Thaçi <jetonthaci@outlook.com>
2019-03-29 15:58:30 +0100

    add two tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,5 +1,6 @@
-# Testing bin/game.rb
+# Testing Tic Tac Toe
 require './lib/board'
+require './lib/game'
 
 RSpec.shared_examples 'check_winner' do |a, b, c|
   before Board do
@@ -7,7 +8,7 @@ RSpec.shared_examples 'check_winner' do |a, b, c|
     @board = @board_class.board
   end
 
-  it 'cheks if we have a winner' do
+  it 'checks if we have a winner' do
     @board[0] = [a, b, c]
     expect(@board_class.check_winner).to be true
   end
@@ -15,8 +16,20 @@ end
 
 RSpec.describe 'tests the input from user' do
   include_examples 'check_winner', 'O', 'O', 'O'
-  include_examples 'check_winner', 'X', 'O', 'O'
-  include_examples 'check_winner', 'O', 'X', 'O'
-  include_examples 'check_winner', nil, 'O', 'O'
   include_examples 'check_winner', 'X', 'X', 'X'
+end
+
+RSpec.shared_examples 'input' do |field|
+  before Game do
+    @game = Game.new
+  end
+
+  it 'sanitizes input' do
+    expect(@game.input).to eql(field)
+  end
+end
+
+RSpec.describe 'tests the input from user' do
+  include_examples 'input', '11'
+  include_examples 'input', '00'
 end
